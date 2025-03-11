d66b24b83b73d9d4700519d7ab72809a0c23f6ac
Paul Knapp <ppankluap@gmail.com>
2025-03-11 18:38:02 -0500

    Feat: add validations and has_many association to models.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,5 @@
 class Customer < ApplicationRecord
-    
+    validates :first_name, :last_name, :email, :address, presence: true
     has_many :subscriptions, dependent: :destroy
+    has_many :teas, through: :subscriptions
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,3 +1,6 @@
 class Subscription < ApplicationRecord
+  validates :title, :price, :status, :frequency, presence: true
   belongs_to :customer
+  has_many :subscription_teas
+  has_many :teas, through: :subscription_teas
 end
