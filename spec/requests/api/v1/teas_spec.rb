afd4a46d82f76f1fc6d6e8732f4d63bd3fe157cb
Paul Knapp <ppankluap@gmail.com>
2025-03-11 22:15:46 -0500

    Test: tea endpoint testing.

--- spec/requests/api/v1/teas_spec.rb
+++ spec/requests/api/v1/teas_spec.rb
@@ -1,18 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Teas", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/api/v1/teas/index"
-      expect(response).to have_http_status(:success)
+  before(:each) do
+    @tea1 = Tea.create!(
+      title: "Green Tea",
+      description: "A refreshing and healthy tea",
+      temperature: 80,
+      brew_time: "2"
+    )
+
+    @tea2 = Tea.create!(
+      title: "Black Tea",
+      description: "A strong and bold tea",
+      temperature: 95,
+      brew_time: "3"
+    )
+  end
+
+  describe "GET /api/v1/teas" do
+    it "returns all teas" do
+      get "/api/v1/teas"
+
+      expect(response).to have_http_status(:ok)
+
+      json = JSON.parse(response.body, symbolize_names: true)
+      expect(json[:data].length).to eq(2)
+      expect(json[:data][0][:attributes][:title]).to eq("Green Tea")
+      expect(json[:data][1][:attributes][:title]).to eq("Black Tea")
     end
   end
 
-  describe "GET /show" do
-    it "returns http success" do
-      get "/api/v1/teas/show"
-      expect(response).to have_http_status(:success)
+  describe "GET /api/v1/teas/:id" do
+    it "returns a single tea" do
+      get "/api/v1/teas/#{@tea1.id}"
+
+      expect(response).to have_http_status(:ok)
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(json[:data][:id]).to eq(@tea1.id.to_s)
+      expect(json[:data][:attributes][:title]).to eq("Green Tea")
+      expect(json[:data][:attributes][:description]).to eq("A refreshing and healthy tea")
+      expect(json[:data][:attributes][:temperature]).to eq(80)
+      expect(json[:data][:attributes][:brew_time]).to eq(2)
     end
   end
-
 end
