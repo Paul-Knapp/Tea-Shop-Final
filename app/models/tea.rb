a8314fe977c960a1bedf7d2a9a5f3fec85433982
Paul Knapp <ppankluap@gmail.com>
2025-03-11 19:34:30 -0500

    Test: oops I forgot to add tea model and model test.

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,2 +1,5 @@
 class Tea < ApplicationRecord
+    has_many :subscription_teas
+    has_many :subscriptions, through: :subscription_teas
+    validates :title,:description, presence: true
 end
--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Tea, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'associations' do
+    should have_many(:subscription_teas)
+    should have_many(:subscriptions).through(:subscription_teas)
+    should validate_presence_of(:title)
+    should validate_presence_of(:description)
+  end
 end
