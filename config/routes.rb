1ed6bae03d8f8a9775d9c36eb3f6e9f62420f37b
Paul Knapp <ppankluap@gmail.com>
2025-03-11 21:48:53 -0500

    Fix: syntax error

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,10 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :subscriptions, only: [:index, :show, :update] do 
+      resources :subscriptions, only: [:index, :show, :update]
+
+      resources :teas, only: [:index, :show]
       resources :customers, only: [:index, :show]
-      end
     end
   end
 end
