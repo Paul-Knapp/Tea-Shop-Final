508915f625e6457d7a8a92d0a6c9dbc861eac807
Paul Knapp <ppankluap@gmail.com>
2025-03-11 21:49:05 -0500

    Feat: two endpoints for search

--- app/controllers/api/v1/teas_controller.rb
+++ app/controllers/api/v1/teas_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::TeasController < ApplicationController
+  
   def index
+    teas = Tea.all
+    render json: TeaSerializer.new(teas).serializable_hash.to_json
   end
 
   def show
+    tea = Tea.find(params[:id])
+    render json: TeaSerializer.new(tea).serializable_hash.to_json
   end
 end
